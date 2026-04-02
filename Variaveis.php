2bd87e13d820dc7f8042841b35bbef2593879123
rickelmy085 <rfeitosamalafaia@gmail.com>
2026-04-02 11:54:13 -0300

    Os teste que fizemos na aula

---
+++ Variaveis.php
@@ -0,0 +1,11 @@
+<?php
+//Agora vamos guarda informações em uma variavel
+$nome = "Rickelmy"; //tipo: String
+$idade = 16; //Tipo: Int
+$matriculaAtiva = true; //Tipo: Boolean
+
+//O sinal "=" representa o operador de atribuição.
+// O ponto "." é o operador de concatenação (Une String e variaveis)
+echo "Aluno(a): ". $nome . "\n";
+echo "Idade: ". $idade;
+?>
---
+++ teste1.php
@@ -0,0 +1,11 @@
+<?php
+//1 Declarar variaveis 
+$nome = "Rickelmy";
+$curso = "Desenvolvimento de Sistemas";
+$semestre = 2;
+
+//2 Saída de dados com concatenação
+echo "Nome do Aluno: " . $nome . "<br>";
+echo "Curso: ". $curso ."<br>";
+echo "Semestre: ". $semestre ." semestre";
+?>
