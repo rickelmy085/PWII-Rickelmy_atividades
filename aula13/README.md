c1f94369d6f9eb69583c2d0505cea9485c93390f
rickelmy <rfeitosamalafaia@gmail.com>
2026-05-05 08:23:22 -0300

    Depois de 500 anos testado isso para dar certo, finalmente deu certo

---
+++ aula13/README.md
@@ -0,0 +1,3 @@
+# Como abrir no navegador?
+Precisamos colocar ela antes no xampp(Dentro do htdocs), abra o explorador de arquvivos e coloque assim C:\xampp\htdocs e coloque esse list.php dentro da pasta aula06
+no navegador pesquise assim: http://localhost/listar.php
---
+++ aula13/lista.php
@@ -0,0 +1,87 @@
+<?php
+// configurtar o banco de dados
+$host = 'localhost';
+$dbname = 'projeto_site';
+$usuario = 'root';
+$senha = '';
+
+// Tentativa de conexão PDO 
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $usuario, $senha);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Se der erro na conexão, o sistema para e avisa
+    die("Erro ao conectar com o banco de dados: " . $e->getMessage ()) ;
+} 
+
+//Aqui sera o o comando do SQL para buscar as informações do no BD
+//Aqui pedirmos para ele trazer as colunas do banco contatos
+$sql = "SELECT id , nome, email, mensagem FROM contatos";
+$stmt = $conn -> prepare($sql);
+$stmt -> execute();
+
+// Agora vamos guarda esses resultados
+// O FETCH_ASSOC ele transforma as informações do bd em um array, assim o PHP consegue ler as informações
+$mensagens = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+    body {
+        font-family: Arial, sans-serif;
+        padding: 20px;
+    }
+
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        margin-top: 20px;
+    }
+
+    th, td {
+        border: 1px solid #ccc;
+        padding: 10px;
+        text-align: left;
+    }
+
+    th {
+        background-color: #333;
+        color: white;
+    }
+
+    tr:nth-child(even) {
+        background-color: #f9f9f9;
+    }
+    </style>
+</head>
+<body>
+    <h2>Mensagem Interceptada(BD)</h2>
+
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Nome/Sobreviventes</th>
+            <th>E-mail de Contato</th>
+            <th>Conteúdo da Mensagem</th>
+        </tr>
+
+        <?php foreach ($mensagens as $linha): ?>
+        <tr>
+            <td><?php echo $linha['id']; ?></td>
+            <td><?php echo $linha['nome']; ?></td>
+            <td><?php echo $linha['email']; ?></td>
+            <td><?php echo $linha['mensagem']; ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+
+    <br>
+    <a href="index.html">Voltar para o Formulário</a>
+    
+</body>
+</html>
