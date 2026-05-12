6d70ddddd0cc060075147f7ab308ba25b1028358
Rickelmy <rfeitosamalafaia@gmail.com>
2026-05-12 08:33:10 -0300

    o sql

---
+++ aula06/projeto_site.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 12/05/2026 às 13:32
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `projeto_site`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `contatos`
+--
+
+CREATE TABLE `contatos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mensagem` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `contatos`
+--
+
+INSERT INTO `contatos` (`id`, `nome`, `email`, `mensagem`) VALUES
+(1, 'alezzin', 'aleezin@gmail.com', 'Olá manito'),
+(5, 'Rickelmy', 'rfeitosaaaaaaaaaa@gmail.com', 'VAmboraaaaaaaaaaaaa'),
+(6, 'Rodrigo', 'mateus.carambola@gmail.com', 'Eu amo falar \"SABORRRR\", pq ele é muito nicho cara'),
+(7, 'Six Seven', 'sixseven@gmail.com', '676767676767676767676767');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `contatos`
+--
+ALTER TABLE `contatos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `contatos`
+--
+ALTER TABLE `contatos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
